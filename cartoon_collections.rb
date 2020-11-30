cdcd299815c70961bf5b186b3a8940edcbee5522
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-30 19:13:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,18 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |el| el.length >= 5}
+  #array.any? { |el| el.length >= 5}
+  array.any? do |str|
+		str.length > 4
+	end 
 end
 
 
 def find_valid_calls(array)
-  planeteer_calls.find {|el| el == "Earth!" or el == "Wind!" or el == "Fire!" or el == "Water!" or el == "Heart!" }
+  
+  array.find do |str|
+		str.include? == valid_calls
+	end 
+  
+  #planeteer_calls.find {|el| el == "Earth!" or el == "Wind!" or el == "Fire!" or el == "Water!" or el == "Heart!" }
 end
