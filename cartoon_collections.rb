378747838a7917b2e5dccb174d2910f0c0930e03
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-30 19:16:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def long_planeteer_calls(array)
   array.any? { |el| el.length >= 5}
 end
 
-
 def find_valid_calls(array)
   array.find {|el| el == "Earth!" or el == "Wind!" or el == "Fire!" or el == "Water!" or el == "Heart!" }
 end
